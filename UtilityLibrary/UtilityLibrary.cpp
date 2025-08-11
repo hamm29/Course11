a821718cf74b6c375d55ae2617741014e1c59036
Hammad <hammad055hammad@gmail.com>
2025-08-11 10:00:54 +0800

    the library is semi-ready

--- UtilityLibrary/UtilityLibrary.cpp
+++ UtilityLibrary/UtilityLibrary.cpp
@@ -7,8 +7,14 @@ using namespace std;
 
 int main()
 {
-
+    int arr[10] = { 32,32,2,1,5,6,32,7,2,2 };
+    clsUtil::Srand();
     cout << clsUtil::GenerateKey();
 
-    cout << "heloo\n";
+    cout << endl;
+    cout << clsUtil::TimesRepeated(32, arr, 10) << "\n";
+    clsUtil::PrintArray(arr, 10);
+
+    system("pause>0");
+
 }
